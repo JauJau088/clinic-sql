a0fc765adb9992c0e24c950cb9912e722457a2cb
camilevahviraki <camilevahviraki@gmail.com>
2022-07-12 15:09:16 +0200

    Add invoices and invoices_items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -14,3 +14,29 @@ CREATE TABLE medical_histories (
     REFERENCES patients(id)
 );
  
+CREATE TABLE invoices (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    total_amount DEC,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT,
+    CONSTRAINT fk_history 
+    FOREIGN KEY (medical_history_id)
+    REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    unit_price DEC,
+    quantity INT,
+    total_price DEC,
+    invoice_id INT,
+    treatment_id INT,
+    CONSTRAINT fk_invoice 
+    FOREIGN KEY (invoice_id)
+    REFERENCES invoices(id),
+
+    CONSTRAINT fk_treatment
+    FOREIGN KEY (treatment_id)
+    REFERENCES treatments(id)
+);
